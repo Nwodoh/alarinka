ac7b095dc70f7ec85da1fb361cb51d832c5e600f
Cross-companion <mickdaniels101@gmail.com>
2024-12-03 13:21:50 +0100

    Last Commit

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 import { useUserContext } from "../../UserContext";
 import styles from "./Profile.module.css";
+import Icons from "../Icons/Icons";
 
 function Profile({ setRedirect }) {
-  const { user, setUser, API_URL } = useUserContext();
+  const { user, setUser, API_URL, bookings, payments, myListings } =
+    useUserContext();
   const { name, email } = user;
 
   async function logout() {
@@ -29,6 +32,23 @@ function Profile({ setRedirect }) {
             className="object-cover w-[100%] h-[15vw]"
             alt=""
           />
+        </div>
+        <div className="flex justify-around mt-6 text-[16px] font-medium text-accent-secondary bg-accent-secondary-light border border-accent-secondary capitalize px-[4px] py-[3px] rounded-md">
+          <Link to={"./bookings"} className="flex gap-1 items-center">
+            <Icons type="booking" iconClasses={"fill-accent-secondary"} />
+            <span>{bookings?.length}</span>
+            <span>Bookings</span>
+          </Link>
+          <Link to={"./payments"} className="flex gap-1 items-center">
+            <Icons type="dollar" iconClasses={"fill-accent-secondary"} />
+            <span>{payments?.length}</span>
+            <span>payments</span>
+          </Link>
+          <Link to={"./places"} className="flex gap-1 items-center">
+            <Icons type="gallery" iconClasses={"fill-accent-secondary"} />
+            <span>{myListings?.length}</span>
+            <span>Listings</span>
+          </Link>
         </div>
         <div className="flex mt-6 gap-3 items-center">
           <div className="flex items-center justify-center w-[8vw] h-[8vw] min-w-[65px] min-h-[65px] bg-primary_light text-primary rounded-full text-4xl uppercase">
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,6 +6,8 @@ export default {
       colors: {
         primary: "#1A365D",
         primary_light: "rgb(226, 232, 240)",
+        ["accent-secondary"]: "#ff7730",
+        ["accent-secondary-light"]: "#ff78302a",
         bg_color: "#6C757D",
         secondary: "#B56A6A",
         accent: "#2D6A4F",
